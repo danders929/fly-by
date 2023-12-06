c2c2aae35f73b9f76b49665945cdeccf9442775c
Dan <sandersda@gmail.com>
2023-12-06 14:41:37 -0600

    added Link to import for AircraftList

--- src/client/features/main/aircraft/AircraftList.jsx
+++ src/client/features/main/aircraft/AircraftList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AircraftList(){
   const navigate = useNavigate();
