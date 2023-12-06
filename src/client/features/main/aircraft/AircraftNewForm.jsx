79ea929758d5a115c58b335dac6a9429aa4d7e66
Dan <sandersda@gmail.com>
2023-12-06 14:41:50 -0600

    added Nav for AircraftNewForm

--- src/client/features/main/aircraft/AircraftNewForm.jsx
+++ src/client/features/main/aircraft/AircraftNewForm.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-// import react from "@vitejs/plugin-react-swc";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function AircraftNewForm(){
-  const navigate = useNavigate;
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/aircraft`); 
+  };
+
   return (
     <>
       <header>
@@ -15,15 +19,13 @@ export default function AircraftNewForm(){
         <h1>Fly-By</h1>
         <h2>New Aircraft Form</h2>
       </header>
-      <body>
-        <form>
-          <label>Tail Number: <input type="text" /></label>
-          <label>Make/Model: <input type="text" /></label>
-          <label>Single Engine: <input type="checkbox" /></label>
-          <label>Hobbs Meter: <input type="number" /></label>
-        </form>
-        <button>Add</button>
-      </body>
+      <form onSubmit={handleSubmit}>
+        <label>Tail Number: <input type="text" /></label>
+        <label>Make/Model: <input type="text" /></label>
+        <label>Single Engine: <input type="checkbox" /></label>
+        <label>Hobbs Meter: <input type="number" /></label>
+        <button type="submit">Add</button>
+      </form>
     </>
   )
 }
