8a6fb3e8f6a1793eba8050909a1c732b2b653497
Dan <sandersda@gmail.com>
2023-12-08 09:44:48 -0600

    added FlightLog Slice.

---
+++ src/client/features/main/pilot/flightLogSlice.js
@@ -0,0 +1,40 @@
+import api from "../../../store/api";
+import { useSelector } from "react-redux";
+import { selectId } from "../../auth/authSlice";
+
+const usrId = useSelector(selectId)
+
+const flightLogApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getflight: builder.query({
+      query: () => ({
+        url: `/flights/`,
+        params: { picId: usrId }
+      }),
+      providesTags: ["Flight"],
+    }),
+
+    createflight: builder.mutation({
+      query: (flightData) => ({
+        url: `/flights/`,
+        method: "POST",
+        body: flightData,
+      }),
+    }),
+
+    updateFlight: builder.mutation({
+      query: (flightData) => ({
+        url: `/flights/${flightData.id}`,
+        method: "PATCH",
+        body: flightData,
+      }),
+      invalidatesTags: ["Flight"],
+    }),
+  }),
+});
+
+export const {
+  useGetFlightQuery,
+  useCreateFlight,
+  useUpdateFlight,
+} = flightLogApi;
