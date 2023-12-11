1211e8b7a7c4cdafe152b3ffb8944225a6b51250
Dan <sandersda@gmail.com>
2023-12-11 11:16:08 -0600

    added storage and slice functionality

--- src/client/features/main/pilot/flightTimesSlice.js
+++ src/client/features/main/pilot/flightTimesSlice.js
@@ -1,15 +1,15 @@
 import api from "../../../store/api";
 import { useSelector } from "react-redux";
-import { selectId } from "../../auth/authSlice";
+import { selectFlightId } from "./flightLogSlice";
 
-const usrId = useSelector(selectId)
+const flightId = useSelector(selectFlightId)
 
 const flightTimesApi = api.injectEndpoints({
   endpoints: (builder) => ({
     getflightTime: builder.query({
       query: () => ({
         url: `/flightTimes/`,
-        params: { picId: usrId }
+        params: { flightId: flightId }
       }),
       providesTags: ["FlightTime"],
     }),
@@ -38,3 +38,117 @@ export const {
   useCreateFlightTimes,
   useUpdateFlightTimes,
 } = flightTimesApi;
+
+const INITIAL_FLIGHT_START = "initialFlightTimeStart"
+const FLIGHT_START = "flightTimeStart"
+const FLIGHT_STOP = "flightTimeStop"
+const FLIGHT_DAY = "dayFlight"
+
+// Reducer that stores the payloads Flight time start in state and session storage
+const storeInitialFlightTimeStart = (state, { payload }) => {
+  state.flightTimeStart = payload.flightTimeStart;
+  sessionStorage.setItem(INITIAL_FLIGHT_START, payload.flightTimeStart);
+}
+
+// Reducer that stores the payloads Flight time start in state and session storage
+const storeFlightTimeStart = (state, { payload }) => {
+  state.flightTimeStart = payload.flightTimeStart;
+  sessionStorage.setItem(FLIGHT_START, payload.flightTimeStart);
+}
+
+// Reducer that stores the payloads Flight time stop in state and session storage
+const storeFlightTimeStop = (state, { payload }) => {
+  state.flightTimeStop = payload.flightTimeStop;
+  sessionStorage.setItem(FLIGHT_STOP, payload.flightTimeStop);
+}
+
+// Reducer that stores whether or not the current flight time tracking is for day/night in state and session storage
+const storeDayFlight = (state, { payload }) => {
+  state.dayFlight = payload.dayFlight;
+  sessionStorage.setItem(FLIGHT_DAY, payload.dayFlight)
+}
+
+const flightTimeSlice = createSlice({
+  name: "flightTime",
+  initialState: {
+    flightTimeStart: "",
+    flightTimeStop: "",
+    dayFlight: true,
+    flightDuration: 0.00,
+    dayFlightDuration: 0.00,
+    nightFlightDuration: 0.00,
+  },
+  reducers: {
+    // Reducer that calculates the difference between flightTimeStart and current time
+    calculateFlightDuration : (state) => {
+      // Retrieve flightTimeStart from session storage
+      const flightTimeStart = sessionStorage.getItem(INITIAL_FLIGHT_START);
+    
+      // Check if flightTimeStart exists
+      if (flightTimeStart) {
+        const currentTime = new Date();
+        const startTime = new Date(flightTimeStart);
+    
+        // Calculate the difference in minutes
+        const timeDifferenceInMinutes = Math.floor((currentTime - startTime) / (1000 * 60));
+    
+        // Format the duration as "HH.00"
+        const hours = Math.floor(timeDifferenceInMinutes / 60);
+        const formattedDuration = `${hours}.00`;
+    
+        // Update the state with the formatted duration
+        state.flightDuration = formattedDuration;
+      }
+    },
+    // Reducer that calculates the difference between dayFlightTimeStart and current time
+    calculateDayFlightDuration : (state) => {
+      const flightTimeStart = sessionStorage.getItem(FLIGHT_START);
+      const isDayflight = sessionStorage.getItem(FLIGHT_DAY);
+    
+      // Check if flightTimeStart exists
+      if (flightTimeStart && isDayflight) {
+        const currentTime = new Date();
+        const startTime = new Date(flightTimeStart);
+    
+        // Calculate the difference in minutes
+        const timeDifferenceInMinutes = Math.floor((currentTime - startTime) / (1000 * 60));
+    
+        // Format the duration as "HH.00"
+        const hours = Math.floor(timeDifferenceInMinutes / 60);
+        const formattedDuration = `${hours}.00`;
+    
+        // Update the state with the formatted duration
+        state.dayFlightDuration += formattedDuration;
+      }
+    },
+
+    calculateNightFlightDuration : (state) => {
+      const flightTimeStart = sessionStorage.getItem(FLIGHT_START);
+      const isDayflight = sessionStorage.getItem(FLIGHT_DAY);
+    
+      // Check if flightTimeStart exists
+      if (flightTimeStart && !isDayflight) {
+        const currentTime = new Date();
+        const startTime = new Date(flightTimeStart);
+    
+        // Calculate the difference in minutes
+        const timeDifferenceInMinutes = Math.floor((currentTime - startTime) / (1000 * 60));
+    
+        // Format the duration as "HH.00"
+        const hours = Math.floor(timeDifferenceInMinutes / 60);
+        const formattedDuration = `${hours}.00`;
+    
+        // Update the state with the formatted duration
+        state.nightFlightDuration += formattedDuration;
+      }
+    }
+  },
+});
+
+export const { calculateFlightDuration, calculateDayFlightDuration, calculateNightFlightDuration } = flightTimeSlice.actions;
+
+export const selectFlightDuration = (state) => state.flightDuration;
+export const selectDayFlightDuration = (state) => state.dayFlightDuration;
+export const selectNightFlightDuration = (state) => state.nightFlightDuration;
+
+export default flightTimeSlice.reducer;
