82b3a10ae79839fefc621d01e2c3a27f4b597092
Dan <sandersda@gmail.com>
2023-12-12 06:26:21 -0600

    added reducers and flight duration interval

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -1,14 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import flightReducer from "../features/main/pilot/flightLogSlice"
+import flightTimeReducer, { calculateDayFlightDuration, calculateFlightDuration, calculateNightFlightDuration } from "../features/main/pilot/flightTimesSlice"
 import api from "./api";
 
 const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     auth: authReducer,
+    flight: flightReducer,
+    flightTime: flightTimeReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 });
+// sets up an interval to dispatch the flight duration actions every minute
+setInterval(() => {
+  store.dispatch(calculateFlightDuration());
+  store.dispatch(calculateDayFlightDuration());
+  store.dispatch(calculateNightFlightDuration());
+}, 60000);
 
 export default store;
