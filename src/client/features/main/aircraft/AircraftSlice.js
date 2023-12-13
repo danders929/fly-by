78b0efccf2eab718681704519533bae067814d83
Dan <sandersda@gmail.com>
2023-12-13 10:48:15 -0600

    added aircraft data to state and session

--- src/client/features/main/aircraft/AircraftSlice.js
+++ src/client/features/main/aircraft/AircraftSlice.js
@@ -1,3 +1,4 @@
+import { createSlice } from "@reduxjs/toolkit";
 import api from "../../../store/api";
 
 const aircraftApi = api.injectEndpoints({
@@ -33,3 +34,35 @@ export const {
   useCreateAircraft,
   useUpdateAircraft,
 } = aircraftApi;
+
+const aircraftSlice = createSlice({
+  name: "aircraft",
+  initialState: {
+    id: "",
+    makeModel: "",
+    tailNum: "",
+  },
+  reducers: {
+    setId: (state, action) => {
+      state.id = action.payload;
+    },
+    setMakeModel: (state, action) => {
+      state.makeModel = action.payload;
+    },
+    setTailnum: (state, action) => {
+      state.tailNum = action.payload;
+    },
+    resetAircraft: (state) => {
+      state.id = "";
+      state.makeModel = "";
+      state.tailNum = "";
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addMatcher(aircraftApi.endpoints.getAircraft.matchFulfilled, (state, action) => {
+      state.id = action.payload.id
+      state.makeModel = action.payload.makeModel;
+      state.tailNum = action.payload.tailNum;
+    });
+  },
+});
